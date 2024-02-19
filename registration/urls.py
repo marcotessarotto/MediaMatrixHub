e2581f2df329f0dfc4ac4b94e67d79d800835716
marco <marcotts@gmail.com>
2024-02-19 17:59:03 +0100

    update

--- registration/urls.py
+++ registration/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.subscriber_login, name='subscriber_login'),
+    path('login/', views.subscriber_login, name='subscriber-login'),
     # Ensure you have paths for 'manage-subscription' and 'login-error' views as well
     path('manage-subscription/', views.manage_subscription, name='manage-subscription'),
+    path('logout/', views.subscriber_logout, name='subscriber-logout'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,12 +1,23 @@
 from django.contrib.auth import login
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
+
+from mediamatrixhub.view_tools import is_private_ip
 from .forms import SubscriberLoginForm, EventParticipationForm
 from .models import Subscriber, InformationEvent, EventParticipation
 
 
 def subscriber_login(request):
+
+    # get ip address from request META
+    http_real_ip = request.META.get('HTTP_X_REAL_IP', '')
+    # print(http_real_ip)
+
+    # Check if the IP is private
+    if http_real_ip != '' and not is_private_ip(http_real_ip):
+        return HttpResponse(status=403)
+
     if request.method == 'POST':
         form = SubscriberLoginForm(request.POST)
         if form.is_valid():
@@ -29,26 +40,21 @@ def subscriber_login(request):
 
 
 def manage_subscription(request):
+
+    # get ip address from request META
+    http_real_ip = request.META.get('HTTP_X_REAL_IP', '')
+    # print(http_real_ip)
+
+    # Check if the IP is private
+    if http_real_ip != '' and not is_private_ip(http_real_ip):
+        return HttpResponse(status=403)
+
     # check request.session['subscriber_id'] and retrieve the subscriber instance
     subscriber_id = request.session.get('subscriber_id')
     if subscriber_id:
         subscriber = Subscriber.objects.get(id=subscriber_id)
     else:
         raise Http404('Utente non trovato')
-
-    # if request.method == 'POST':
-    #     form = EventParticipationForm(request.POST)
-    #     if form.is_valid():
-    #         # Assuming the user's subscriber information is available, e.g., through user profile
-    #         subscriber = Subscriber.objects.get(email=request.user.email)
-    #         for key, value in form.cleaned_data.items():
-    #             if value:  # Checkbox is checked
-    #                 event_id = key.split('_')[-1]
-    #                 event = InformationEvent.objects.get(id=event_id)
-    #                 EventParticipation.objects.create(event=event, subscriber=subscriber)
-    #         return redirect('success_page')  # Redirect to a new URL
-    # else:
-    #     form = EventParticipationForm()
 
     if request.method == 'POST':
         form = EventParticipationForm(request.POST)
@@ -79,5 +85,10 @@ def manage_subscription(request):
 
     # return render(request, 'events/event_participation.html', {'form': form})
 
-
     return render(request, 'subscribers/manage_subscription.html', {'subscriber': subscriber, 'form': form})
+
+
+def subscriber_logout(request):
+    request.session['subscriber_id'] = None
+
+    return redirect('subscriber-login')
