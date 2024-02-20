a7bd750136c0ae7fc69b81ebba3e7e6508404624
marco <marcotts@gmail.com>
2024-02-20 18:13:49 +0100

    update

--- registration/management/commands/list_enabled_events.py
+++ registration/management/commands/list_enabled_events.py
@@ -1,5 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.db.models import Count
+from django.utils import formats
+
 from registration.models import InformationEvent
 
 
@@ -8,11 +10,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # Fetch enabled events with participation count
-        enabled_events_with_counts = InformationEvent.enabled_events.with_participation_count()
+        enabled_events_with_counts = InformationEvent.enabled_events.with_participation_count().order_by(
+            'event_date', 'event_start_time')
 
         if enabled_events_with_counts:
             self.stdout.write(self.style.SUCCESS('Enabled events with participation counts:'))
             for event in enabled_events_with_counts:
-                self.stdout.write(f"{event.title}: {event.participation_count} participations")
+                formatted_event_date = formats.date_format(event.event_date, "l j F Y") if event.event_date else "N/A"
+
+                self.stdout.write(f"#{event.id}, {event.title}, {formatted_event_date}: {event.participation_count} iscrizioni")
         else:
             self.stdout.write(self.style.WARNING('No enabled events found.'))
