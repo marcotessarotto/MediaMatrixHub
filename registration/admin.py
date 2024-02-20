e3d93ab8b8fb5a3c74f452a1a5412519c72ddb78
marco <marcotts@gmail.com>
2024-02-20 12:09:09 +0100

    update

--- registration/admin.py
+++ registration/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.http import HttpResponse
+from openpyxl.workbook import Workbook
+
+from mediamatrixhub.admin_utils import ExportExcelMixin
 from .models import InformationEvent, Subscriber, EventParticipation, EventLog
 
 
@@ -22,9 +26,55 @@ class SubscriberAdmin(admin.ModelAdmin):
 
 @admin.register(EventParticipation)
 class EventParticipationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'event', 'subscriber', 'created_at', )
-    list_filter = ('event', 'subscriber')
+    list_display = ('id', 'event', 'subscriber', 'created_at', 'get_subscriber_email')
+    list_filter = ('event', 'created_at',)
     search_fields = ('event__title', 'subscriber__name', 'subscriber__surname')
+
+    actions = ["export_as_excel"]
+
+    def get_subscriber_email(self, obj):
+        return obj.subscriber.email
+    get_subscriber_email.admin_order_field = 'subscriber__email'  # Allows column order sorting
+    get_subscriber_email.short_description = 'Subscriber Email'  # Renames column head
+
+    def export_as_excel(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        # from field_names, remove 'created_at' and 'updated_at' fields
+        field_names.remove('created_at')
+        field_names.remove('updated_at')
+
+        # Manually add the names of any custom method fields you want to include
+        custom_methods = ['get_subscriber_email']
+
+        response = HttpResponse(content_type='application/ms-excel')
+        response['Content-Disposition'] = f'attachment; filename={meta}.xlsx'
+        wb = Workbook()
+        ws = wb.active
+
+        # Include custom method names in the header row
+        ws.append(field_names + custom_methods)
+
+        for obj in queryset:
+            row = []
+            for field in field_names:
+                value = getattr(obj, field)
+                row.append(value if isinstance(value, str) else str(value))
+
+            # Manually get values from custom methods
+            for method in custom_methods:
+                if hasattr(self, method):
+                    method_value = getattr(self, method)(obj)
+                    row.append(method_value)
+
+            ws.append(row)
+
+        wb.save(response)
+        return response
+
+    export_as_excel.short_description = "Export Selected as Excel"
+
 
 
 @admin.register(EventLog)
