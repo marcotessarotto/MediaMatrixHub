7ececdd15cf7f6b729034527e0eeabc20690a72a
marco <marcotts@gmail.com>
2024-03-28 11:14:51 +0100

    update

--- static/assets/template/js/main.js
+++ static/assets/template/js/main.js
@@ -74,12 +74,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }));
   }
 
-  /**
-   * Initiate glightbox
-   */
-  const glightbox = GLightbox({
-    selector: '.glightbox'
-  });
 
 //
 
