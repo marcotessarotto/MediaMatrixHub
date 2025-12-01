271f307fef1c12b5eb8cb2509429591bdc99486d
marco <marcotts@gmail.com>
2025-12-01 17:22:14 +0100

    update

--- registration/json_tools.py
+++ registration/json_tools.py
@@ -34,7 +34,6 @@ def parse_json_file_to_dict_by_matricola(file_name):
 
 __preloaded_dict = {}
 
-
 def lookup_subscriber_json_data_by_matricola(matricola):
     """
     Lookup a person by their 'matricola'.
@@ -50,6 +49,162 @@ def lookup_subscriber_json_data_by_matricola(matricola):
         __preloaded_dict = parse_json_file_to_dict_by_matricola('registration/res/persfvg_dump_persone_entita.json')
     return __preloaded_dict.get(matricola, None)
 
-# __preloaded_
-#
-# def get_dump_final():
+__preloaded_dict2 = {}
+
+def get_dump_final_json():
+    global __preloaded_dict2
+    if not __preloaded_dict2:
+        file_name = 'registration/res/persfvg_dump_final.json'
+
+        try:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+            return data
+        except FileNotFoundError:
+            print(f"The file {file_name} was not found.")
+            return {}
+        except json.JSONDecodeError:
+            print("Failed to decode JSON, please check the file format.")
+            return {}
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return {}
+
+    return __preloaded_dict2
+
+__preloaded_dict3 = {}
+
+def get_dump_persone_entita_json():
+    global __preloaded_dict3
+    if not __preloaded_dict3:
+        file_name = 'registration/res/persfvg_dump_persone_entita.json'
+
+        try:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+            return data
+        except FileNotFoundError:
+            print(f"The file {file_name} was not found.")
+            return {}
+        except json.JSONDecodeError:
+            print("Failed to decode JSON, please check the file format.")
+            return {}
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return {}
+
+    return __preloaded_dict3
+
+
+__parent_structure = {}
+__children_structures = {}
+__structure_name = {}
+__preloaded_data = False
+
+def get_uaf_children_recursively(uaf):
+
+    global __parent_structure, __children_structures, __structure_name, __preloaded_data
+
+    if not __preloaded_data:
+        __parent_structure = {}
+
+        __children_structures = {}
+
+        __structure_name = {}
+
+        data2 = get_dump_final_json()
+
+        for k, v in data2.items():
+
+            # print(f"key: {k}")
+            # print(f"value: {v[3]}")
+
+            parent_uaf = v[3]
+
+            # key is the uaf
+            # parent uaf is v[3]
+
+            __parent_structure[k] = parent_uaf
+            __structure_name[k] = v[0]
+
+            if parent_uaf not in __children_structures:
+                __children_structures[parent_uaf] = [k]
+            else:
+                if k != parent_uaf:
+                    if k not in __children_structures[parent_uaf]:
+                        __children_structures[parent_uaf].append(k)
+
+    # print()
+    # # print(children_structures)
+    # for k, v in __children_structures.items():
+    #     print(f"{k}: {v}")
+    # print()
+    # print()
+    #
+    # for k, v in __parent_structure.items():
+    #     print(f"{k}: {v}")
+    # # print number of keys in
+    #
+    # print()
+    # for k, v in __structure_name.items():
+    #     print(f"{k}: {v}")
+
+    # given a uaf, return recursive list of children uafs and itself
+    def get_recursive_children(uaf):
+
+        result = []
+        result2 = []
+
+        if uaf in __children_structures:
+            result = __children_structures[uaf]
+
+            for uaf2 in result:
+                result2.extend(get_recursive_children(uaf2))
+
+        # join result and result2
+        result.extend(result2)
+        # add uaf itself to the result
+        result.append(uaf)
+
+        # return a list of unique uafs
+        result = list(set(result))
+
+        return result
+
+    r = get_recursive_children(uaf)
+
+    # print(f"uaf {uaf} and its children: {r}")
+    # print(f"length: {len(r)}")
+
+    return r
+
+
+
+def get_all_employees_uaf(uaf):
+    list_of_uafs = get_uaf_children_recursively(uaf)
+
+    print(f"length of list_of_uafs: {len(list_of_uafs)}")
+
+    dump_persone_entita = get_dump_persone_entita_json()
+
+    print(f"length of dump_persone_entita: {len(dump_persone_entita)}")
+
+    employees = []
+
+    for k,v in dump_persone_entita.items():
+        # print(k)
+        # print(v)
+        try:
+            employee_uaf = v[6]
+
+            if not employee_uaf:
+                print(f"skipped(2): {k}")
+                continue
+
+            if employee_uaf in list_of_uafs:
+                employees.append(v)
+        except IndexError:
+            print(f"skipped: {k}")
+            pass
+
+    return employees
--- registration/management/commands/show_sub_structures.py
+++ registration/management/commands/show_sub_structures.py
@@ -2,6 +2,7 @@ import json
 
 from django.core.management import BaseCommand
 
+from registration.json_tools import get_uaf_children_recursively, get_all_employees_uaf
 from registration.logic import create_subscriber
 from registration.models import Subscriber
 
@@ -19,99 +20,116 @@ class Command(BaseCommand):
         print(f"Processing UAF: {uaf}")
 
 
+        if False:
+            with open(file_path2, "r") as file:
+                # read json from file
+                json_str2 = file.read()
 
-        with open(file_path2, "r") as file:
-            # read json from file
-            json_str2 = file.read()
+                # parse json string
+                data2 = json.loads(json_str2)
 
-            # parse json string
-            data2 = json.loads(json_str2)
-
-        print(f"Loaded data from file: {data2}")
-
-
-        counter = 0
-
-        not_valid = {}
-
-        matricola_email = {}
-
-        parent_structure = {}
-
-        children_structures = {}
-
-        structure_name = {}
-
-        for k, v in data2.items():
-
-            print(f"key: {k}")
-            print(f"value: {v[3]}")
-
-            parent_uaf = v[3]
-
-            # key is the uaf
-            # parent uaf is v[3]
-
-            parent_structure[k] = parent_uaf
-            structure_name[k] = v[0]
-
-            if parent_uaf not in children_structures:
-                children_structures[parent_uaf] = [k]
-            else:
-                if k != parent_uaf:
-                    if k not in children_structures[parent_uaf]:
-                       children_structures[parent_uaf].append(k)
+            print(f"Loaded data from file: {data2}")
 
 
-        print()
-        # print(children_structures)
-        for k, v in children_structures.items():
-            print(f"{k}: {v}")
-        print()
-        print()
+            parent_structure = {}
 
-        for k,v in parent_structure.items():
-            print(f"{k}: {v}")
-        # print number of keys in
+            children_structures = {}
 
-        print()
-        for k,v in structure_name.items():
-            print(f"{k}: {v}")
+            structure_name = {}
 
+            for k, v in data2.items():
 
-        # given a uaf, return recursive list of children uafs and itself
-        def get_recursive_children(uaf):
+                print(f"key: {k}")
+                print(f"value: {v[3]}")
 
-            def get_uaf_children(uaf):
-                try:
-                    return children_structures[uaf]
-                except KeyError:
-                    return []
+                parent_uaf = v[3]
 
-            result = []
-            result2 = []
+                # key is the uaf
+                # parent uaf is v[3]
 
-            if uaf in children_structures:
-                result = children_structures[uaf]
+                parent_structure[k] = parent_uaf
+                structure_name[k] = v[0]
 
-                for uaf2 in result:
-                    result2.extend(get_uaf_children(uaf2))
+                if parent_uaf not in children_structures:
+                    children_structures[parent_uaf] = [k]
+                else:
+                    if k != parent_uaf:
+                        if k not in children_structures[parent_uaf]:
+                           children_structures[parent_uaf].append(k)
+
 
             print()
-            print(result)
-            print(result2)
+            # print(children_structures)
+            for k, v in children_structures.items():
+                print(f"{k}: {v}")
+            print()
+            print()
 
-            # join result and result2
-            result.extend(result2)
-            # add uaf itself to the result
-            result.append(uaf)
+            for k,v in parent_structure.items():
+                print(f"{k}: {v}")
+            # print number of keys in
 
-            return result
+            print()
+            for k,v in structure_name.items():
+                print(f"{k}: {v}")
 
 
-        r = get_recursive_children(uaf)
+            # given a uaf, return recursive list of children uafs and itself
+            def get_recursive_children(uaf):
+
+                def get_uaf_children(uaf):
+                    try:
+                        return children_structures[uaf]
+                    except KeyError:
+                        return []
+
+                result = []
+                result2 = []
+
+                if uaf in children_structures:
+                    result = children_structures[uaf]
+
+                    for uaf2 in result:
+                        result2.extend(get_uaf_children(uaf2))
+
+                print()
+                print(result)
+                print(result2)
+
+                # join result and result2
+                result.extend(result2)
+                # add uaf itself to the result
+                result.append(uaf)
+
+                return result
+
+            print()
+            print()
+            print()
+
+            r = get_recursive_children(uaf)
+
+            print(f"uaf {uaf} and its children: {r}")
+            print(f"length: {len(r)}")
+
+            print()
+            print()
+
+        r = get_uaf_children_recursively(uaf)
 
         print(f"uaf {uaf} and its children: {r}")
         print(f"length: {len(r)}")
+
+        print()
+        print()
+
+        list_of_employees = get_all_employees_uaf(uaf)
+
+        # print(f"Employees: {list_of_employees}")
+
+        print(f"Number of employees: {len(list_of_employees)}")
+
+        if len(list_of_employees) < 30:
+            print(list_of_employees)
 
         # return result
